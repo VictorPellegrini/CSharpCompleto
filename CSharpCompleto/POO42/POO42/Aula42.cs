e7b48286c2b740abfac80b1abd1ecbd4bfbdea87
Victor Pellegrini <victor.pellegrini@carambola.com.vc>
2021-10-05 08:59:18 -0300

    adequação e criação da aula42

---
+++ CSharpCompleto/POO42/POO42/Aula42.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace POO42
+{
+    class Aula42
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto;
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.WriteLine("Nome:");
+            p1.Nome = Console.ReadLine();
+            Console.WriteLine("Preço:");
+            p1.Preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade:");
+            p1.Quantidade = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ CSharpCompleto/POO42/POO42/Produto.cs
@@ -0,0 +1,26 @@
+﻿namespace POO42
+{
+    class Produto
+    {
+        public string Nome;
+        public int Quantidade;
+        public double Preco;
+
+        public double Valor_Em_Estoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public int Add_Produto()
+        {
+            Quantidade = Quantidade + Adicionado;
+            return Quantidade;
+        }
+
+        public int Remove_Produto()
+        {
+            Quantidade = Quantidade - Removido;
+            return Quantidade;
+        }
+    }
+}
