2d4046ba70bf2c3c95016d7b1fc93cc5f9ef35e4
Victor Pellegrini <victor.pellegrini@carambola.com.vc>
2021-10-04 22:52:47 -0300

    rename project

--- CSharpCompleto/POO/Aula40.cs
+++ CSharpCompleto/POO/Aula40.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace POO
+namespace POO40
 {
     class Aula40
     {
--- CSharpCompleto/POO/Pessoas.cs
+++ CSharpCompleto/POO/Pessoas.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace POO
+namespace POO40
 {
     class Pessoas
     {
