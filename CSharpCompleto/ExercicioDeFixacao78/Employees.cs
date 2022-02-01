1c709aa8934fb719a60983a39a4e1488fd604b1a
Victor Pellegrini <victor.pellegrini@ambevtech.com.br>
2022-01-31 21:41:39 -0300

    Exercício de fixação 78

---
+++ CSharpCompleto/ExercicioDeFixacao78/Employees.cs
@@ -0,0 +1,21 @@
+﻿namespace ExercicioDeFixacao78
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employees(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void SalaryIncrease(double percent)
+        {
+            Salary = Salary + (Salary * percent / 100);
+        }
+    }
+}
---
+++ CSharpCompleto/ExercicioDeFixacao78/UserStory78.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioDeFixacao78
+{
+    public class UserStory78
+    {
+        static void Main()
+        {
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employees> employeesList = new List<Employees>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\r\nEmployee #" + (i + 1) + ":");
+
+                Console.Write("Insert ID: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Insert name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Insert salary: $");
+                double salary = double.Parse(Console.ReadLine());
+
+                Employees employee = new Employees(id, name, salary);
+
+                employeesList.Add(employee);
+            }
+
+            Console.Write("\r\nInsert the employee's ID that will have a salary increase: ");
+            int idSalaryIncrease = int.Parse(Console.ReadLine());
+
+            if (employeesList.Exists(x => x.Id == idSalaryIncrease) == true)
+            {
+                Console.Write("\r\nInsert the salary increase percent: ");
+                double percent = double.Parse(Console.ReadLine());
+
+                employeesList.Find(x => x.Id == idSalaryIncrease).SalaryIncrease(percent);
+            } 
+            else
+                Console.WriteLine("This employee does not exist");
+
+            foreach (Employees employee in employeesList)
+            {
+                Console.WriteLine("\r\nEmployee: ");
+                Console.WriteLine("- ID: " + employee.Id);
+                Console.WriteLine("- Name: " + employee.Name);
+                Console.WriteLine("- Salary: $" + employee.Salary.ToString("F2"));
+            }
+        }
+    }
+}
