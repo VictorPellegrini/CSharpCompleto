17bc0f40ea76c6169bb8f6e79a925e00af9a4be8
Victor Pellegrini <victor.pellegrini@ambevtech.com.br>
2022-04-01 19:36:00 -0300

    Iniciando exercicio 132

---
+++ CSharpCompleto/ExercicioDeFixacao132_EnumeracaoComposicao/UserStory132.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioDeFixacao132_EnumeracaoComposicao
+{
+    public class UserStory132
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
