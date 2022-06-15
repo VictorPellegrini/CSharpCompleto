5145d110b237e0888810fdac02bf206bcbdd02e1
Victor Pellegrini <victor.pellegrini@ambevtech.com.br>
2022-06-15 12:13:35 -0300

    corrigindo nomenclatura e criando novo projeto

---
+++ CSharpCompleto/Section15223_GenericsSetDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Section15223_GenericsSetDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
