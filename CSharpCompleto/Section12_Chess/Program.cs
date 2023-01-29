4e171cba36212211bd9f3095c0f8af91e9a955d7
Victor Pellegrini <victor.pellegrini@ambevtech.com.br>
2023-01-29 20:29:35 -0300

    classe posição criada

---
+++ CSharpCompleto/Section12_Chess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Section12_Chess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
