0029fce855ffd4c1f499bdcbed00787ef44a8a22
Victor Pellegrini <victor.pellegrini@ambevtech.com.br>
2023-08-10 15:12:37 -0300

    criação do peão

---
+++ CSharpCompleto/Section12_Chess/Pieces/Pawn.cs
@@ -0,0 +1,17 @@
+﻿using Section12_Chess.GameBoard;
+using System;
+
+namespace Section12_Chess.Pieces
+{
+    public class Pawn : Piece
+    {
+        public Pawn(Board board, Color color) : base(board, color)
+        {
+        }
+
+        public override bool[,] PossibleMovements()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
